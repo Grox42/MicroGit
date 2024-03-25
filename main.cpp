2364d8dba26286183142784a73be15ae7aa8f499
Yudin Roman <r.v.yudin@viarune.com>
2024-03-25 16:36:46 +0700

    added files

--- main.cpp
+++ main.cpp
@@ -11,5 +11,9 @@ int main(int argc, char *argv[])
     ConsoleWriter consoleWriter;
     QObject::connect(&timer, &QTimer::timeout, &fileManager, &FileManager::updateing);
     QObject::connect(&fileManager, &FileManager::modified, &consoleWriter, &ConsoleWriter::write);
+    fileManager.addFile("../resources/file1.txt");
+    fileManager.addFile("../resources/file2.txt");
+    fileManager.addFile("../resources/file3.txt");
+    timer.start(100);
     return a.exec();
 }
