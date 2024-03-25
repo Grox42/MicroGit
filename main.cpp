43aa5b2e7edaa63b16f5d040f49af57cd3059682
Yudin Roman <r.v.yudin@viarune.com>
2024-03-26 02:38:21 +0700

    implemented: Logger

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "filemanager.h"
 #include "consolewriter.h"
+#include "logger.h"
 #include <QCoreApplication>
 #include <QTimer>
 
@@ -9,11 +10,16 @@ int main(int argc, char *argv[])
     QTimer timer;
     FileManager fileManager;
     ConsoleWriter consoleWriter;
+    Logger logger;
+
     QObject::connect(&timer, &QTimer::timeout, &fileManager, &FileManager::updateing);
     QObject::connect(&fileManager, &FileManager::modified, &consoleWriter, &ConsoleWriter::write);
+    QObject::connect(&fileManager, &FileManager::modified, &logger, &Logger::log);
+
     fileManager.addFile("../resources/file1.txt");
     fileManager.addFile("../resources/file2.txt");
     fileManager.addFile("../resources/file3.txt");
+
     timer.start(100);
     return a.exec();
 }
