12934f3bc344cf3039976ad5def925f68a47a7d7
Yudin Roman <r.v.yudin@viarune.com>
2024-03-25 21:31:53 +0700

    removed: pos

--- consolewriter.h
+++ consolewriter.h
@@ -13,7 +13,6 @@ private:
     QVector<QString> title { "index", "Name", "Status", "Size", "Path" };
     QVector<qint32> sizes {        6,     18,       12,     12,     60 };
     HANDLE hundle {GetStdHandle(STD_OUTPUT_HANDLE)};
-    COORD pos {0, 0};
 public:
     ConsoleWriter(QObject *parent = nullptr);
     QString sizeToString(qint64 size) const;
