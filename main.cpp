dcaf663b38601fc0f8308f8edd963a26f4674cd7
Yudin Roman <r.v.yudin@viarune.com>
2024-03-26 19:50:31 +0700

    fixed: main

--- main.cpp
+++ main.cpp
@@ -8,17 +8,22 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QTimer timer;
-    FileManager fileManager;
     ConsoleWriter consoleWriter;
     Logger logger;
 
-    QObject::connect(&timer, &QTimer::timeout, &fileManager, &FileManager::updateing);
-    QObject::connect(&fileManager, &FileManager::modified, &consoleWriter, &ConsoleWriter::write);
-    QObject::connect(&fileManager, &FileManager::modified, &logger, &Logger::log);
+    QObject::connect(
+        &timer, &QTimer::timeout,
+        &FileManager::getInstance(),&FileManager::updateing);
+    QObject::connect(
+        &FileManager::getInstance(), &FileManager::modified,
+        &consoleWriter, &ConsoleWriter::write);
+    QObject::connect(
+        &FileManager::getInstance(), &FileManager::modified,
+        &logger, &Logger::log);
 
-    fileManager.addFile("../resources/file1.txt");
-    fileManager.addFile("../resources/file2.txt");
-    fileManager.addFile("../resources/file3.txt");
+    FileManager::getInstance().addFile("resources/file1.txt");
+    FileManager::getInstance().addFile("resources/file2.txt");
+    FileManager::getInstance().addFile("resources/file3.txt");
 
     timer.start(100);
     return a.exec();
